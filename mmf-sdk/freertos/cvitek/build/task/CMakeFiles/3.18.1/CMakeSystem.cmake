07701a12d569a0a8212041947e30ceb2bbe466ff
Sophgo-Zhenhuihuang <zhenhui.huang@sophgo.com>
2022-10-12 23:55:05 +0800

    add freerots build

---
+++ mmf-sdk/freertos/cvitek/build/task/CMakeFiles/3.18.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-4.15.0-193-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "4.15.0-193-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/data/zhenhui.huang_sophgo/sophpi-huashan_0/mmf-sdk/freertos/cvitek/scripts/toolchain-riscv64-elf.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "riscv64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)

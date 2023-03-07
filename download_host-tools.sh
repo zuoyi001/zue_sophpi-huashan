796773a61256e725cfacb103e132e30ec81d93e9
wangliang.wang <wangliang.wang@sophgo.com>
2023-03-07 17:35:34 +0800

    fix build error.

--- download_host-tools.sh
+++ download_host-tools.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget https://sophon-file.sophon.cn/sophon-prod-s3/drive/23/02/24/11/host-tools.tar.gz
+wget https://sophon-file.sophon.cn/sophon-prod-s3/drive/23/03/07/16/host-tools.tar.gz
 #tar -xvf host-tools.tar.gz
